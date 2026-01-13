15fce824b09b142ddd0ddc969191dbf4d9134fe0
donutnomad <143981763+donutnomad@users.noreply.github.com>
2026-01-13 22:52:34 +0800

    refactor(db): streamline DB interface by removing unused methods

--- internal/interfaces/db.go
+++ internal/interfaces/db.go
@@ -3,24 +3,15 @@ package interfaces
 import (
 	"context"
 	"database/sql"
+
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type DB interface {
 	Model(value any) (tx *gorm.DB)
-	Create(value any) (tx *gorm.DB)
-	Save(value any) (tx *gorm.DB)
 	Where(query any, args ...any) (tx *gorm.DB)
 	Table(name string, args ...any) (tx *gorm.DB)
-	Delete(value any, conds ...any) (tx *gorm.DB)
-	Transaction(fn func(tx *gorm.DB) error, opts ...*sql.TxOptions) error
 	AutoMigrate(table ...any) error
-	Unscoped() *gorm.DB
-	FirstOrCreate(dest any, conds ...any) (tx *gorm.DB)
-	First(dest any, conds ...any) (tx *gorm.DB)
-	Scopes(funcs ...func(*gorm.DB) *gorm.DB) (tx *gorm.DB)
-	Clauses(conds ...clause.Expression) (tx *gorm.DB)
 	Raw(sql string, values ...any) (tx *gorm.DB)
 	Exec(sql string, values ...any) (tx *gorm.DB)
 	WithContext(ctx context.Context) *gorm.DB
